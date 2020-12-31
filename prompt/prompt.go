a558977447b746a8ba247b650e0c51cfcfa97a22
mritd <mritd@linux.com>
2020-12-31 13:54:17 +0800

    feat(prompt): add comment
    add comment
    
    Signed-off-by: mritd <mritd@linux.com>

--- prompt/prompt.go
+++ prompt/prompt.go
@@ -23,6 +23,7 @@ const (
 // EchoMode sets the input behavior of the text input field.
 type EchoMode int
 
+// copy from textinput.Model
 const (
 	// EchoNormal displays text as is. This is the default behavior.
 	EchoNormal EchoMode = iota
@@ -36,15 +37,39 @@ const (
 	EchoNone
 )
 
+// Model is a data container used to store TUI status information,
+// the ui rendering success style is as follows:
+//
+//	✔ Please Input: aaaa
 type Model struct {
-	CharLimit         int
-	Width             int
-	Prompt            string
-	PromptColor       string
-	ValidateFunc      func(string) error
-	ValidateOkPrefix  string
+	// CharLimit is the maximum amount of characters this input element will
+	// accept. If 0 or less, there's no limit.
+	CharLimit int
+
+	// Width is the maximum number of characters that can be displayed at once.
+	// It essentially treats the text field like a horizontally scrolling
+	// viewport. If 0 or less this setting is ignored.
+	Width int
+
+	// Prompt is the prefix of the prompt library, the user needs to define
+	// the format(including spaces)
+	Prompt string
+
+	// PromptColor defines the color of the prompt prefix
+	PromptColor string
+
+	// ValidateFunc is a "real-time verification" function, which verifies
+	// whether the terminal input data is legal in real time
+	ValidateFunc func(string) error
+
+	// ValidateOkPrefix is the prompt prefix when the validation fails
+	ValidateOkPrefix string
+
+	// ValidateErrPrefix is the prompt prefix when the verification is successful
 	ValidateErrPrefix string
-	EchoMode          int
+
+	// EchoMode sets the input behavior of the text input field.
+	EchoMode EchoMode
 
 	init    bool
 	showErr bool
@@ -52,6 +77,8 @@ type Model struct {
 	err     error
 }
 
+// initData initialize the data model, set the default value and
+// fix the wrong parameter settings during initialization
 func (m *Model) initData() {
 	m.input = textinput.NewModel()
 	if m.CharLimit == 0 {
@@ -86,10 +113,13 @@ func (m *Model) initData() {
 	m.init = true
 }
 
+// Init performs some io initialization actions
 func (m Model) Init() tea.Cmd {
 	return textinput.Blink
 }
 
+// Update method responds to various events and modifies the data model
+// according to the corresponding events
 func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	if !m.init {
@@ -104,11 +134,14 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case tea.KeyEsc:
 			return m, tea.Quit
 		case tea.KeyEnter:
+			// When press the Enter button, if there is a verification error,
+			// an error message is displayed.
 			m.showErr = true
 			if m.err == nil {
 				return m, tea.Quit
 			}
 		case tea.KeyRunes:
+			// Hide verification failure message when entering content again
 			m.showErr = false
 			m.err = nil
 		}
@@ -116,15 +149,18 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	// We handle errors just like any other message
 	case error:
 		m.err = msg
+		m.showErr = true
 		return m, nil
 	}
 
 	m.input, cmd = m.input.Update(msg)
+	// Perform real-time verification function after each input
 	m.err = m.ValidateFunc(m.input.Value())
 
 	return m, cmd
 }
 
+// View reads the data state of the data model for rendering
 func (m Model) View() string {
 	var prefix, prompt, errMsg string
 	if m.err != nil {
@@ -146,8 +182,10 @@ func (m *Model) Value() string {
 	return m.input.Value()
 }
 
+// VFDoNothing is a verification function that does nothing
 func VFDoNothing(_ string) error { return nil }
 
+// VFNotBlank is a verification function that checks whether the input is empty
 func VFNotBlank(s string) error {
 	if strings.TrimSpace(s) == "" {
 		return errors.New("input is empty")
